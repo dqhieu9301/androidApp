4db0e2b5b1fd8e38f892b3a0d57e201c1ab2656c
KieuKhanhDuy <you@example.com>
2023-05-07 21:11:51 +0700

    add class UserFragment

--- app/src/main/java/com/example/appandroid/ui/user/UserFragment.java
+++ app/src/main/java/com/example/appandroid/ui/user/UserFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,6 +13,14 @@ import androidx.fragment.app.Fragment;
 import com.example.appandroid.R;
 
 public class UserFragment extends Fragment {
+
+    private EditText edtTen;
+    private EditText edtDiachi;
+    private EditText edtsdt;
+    private Button btndn;
+    private Button btndx;
+    private  Button btnfalse;
+    private  Button btntrue;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
